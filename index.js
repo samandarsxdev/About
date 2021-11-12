d909cc2db62f7be7e97538d5afd2fea0c28b4806
samandarsxdev <93664571+samandarsxdev@users.noreply.github.com>
2021-11-12 05:51:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// function MyFunction(){
+//     let num = 9;
+//     let V = num * num * num;
+//     console.log(V);
+// }
+  
+// MyFunction("v")
+
+// function getFunction(x,y){
+    
+//     return y=x*x*x*x*x*x*3-6*x*x-7;
+    
+// }
+
+// function playingFootball(player, calc){
+//     console.log(`helllo ${player}`);
+//     calc();
+// }
+
+// playingFootball("Mosalah", function(){
+//     console.log("his shot 100 goals");
+// });
